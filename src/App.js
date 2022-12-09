13fd32c2ce667fdc60ccba2b0cfe1fca00411f22
LuciaRyan <lcryan06@gmail.com>
2022-12-09 12:25:16 +0100

    formatted App.js

--- src/App.js
+++ src/App.js
@@ -3,42 +3,49 @@ import './App.css';
 
 function App() {
     return (
-        <main>
-            <>
-                <article className="product">
-                    <img src="https://www.veggipedia.nl/assets/Uploads/Products/4a69d3b5ad/Citroen-fruit-veggipedia.png"
-                         alt='picture-of-a-lemon'/>
-                    <h2>"Citroen"</h2>
-                    <p>
-                        "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
-                        Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
-                        Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-                    </p>
-                </article>
+        <>
+            <header>
+                <main>
+                    <>
+                        <article className="product">
+                            <img
+                                src="https://www.veggipedia.nl/assets/Uploads/Products/4a69d3b5ad/Citroen-fruit-veggipedia.png"
+                                alt='picture-of-a-lemon'/>
+                            <h2>"Citroen"</h2>
+                            <p>
+                                "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
+                                Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
+                                Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+                            </p>
+                        </article>
 
-                <article className="product">
-                    <img src="https://www.veggipedia.nl/assets/Uploads/Products/4a69d3b5ad/Citroen-fruit-veggipedia.png"
-                         alt='picture-of-a-lemon'/>
-                    <h2>"Limoen"</h2>
-                    <p>
-                        Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten
-                        (Wijnruitfamilie).
-                        Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
-                    </p>
-                </article>
+                        <article className="product">
+                            <img
+                                src="https://www.veggipedia.nl/assets/Uploads/Products/4a69d3b5ad/Citroen-fruit-veggipedia.png"
+                                alt='picture-of-a-lemon'/>
+                            <h2>"Limoen"</h2>
+                            <p>
+                                Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten
+                                (Wijnruitfamilie).
+                                Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
+                            </p>
+                        </article>
 
-                <article className="product">
-                    <img src="https://www.veggipedia.nl/assets/Uploads/Products/4a69d3b5ad/Citroen-fruit-veggipedia.png"
-                         alt='picture-of-a-lemon'/>
-                    <h2>"Ijsblokjes"</h2>
-                    <p>
-                        Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
-                        Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen.
-                        Als het goed is ziet jouw webpagina er nu zo uit:
-                    </p>
-                </article>
-            </>
-        </main>
+                        <article className="product">
+                            <img
+                                src="https://www.veggipedia.nl/assets/Uploads/Products/4a69d3b5ad/Citroen-fruit-veggipedia.png"
+                                alt='picture-of-a-lemon'/>
+                            <h2>"Ijsblokjes"</h2>
+                            <p>
+                                Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
+                                Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen.
+                                Als het goed is ziet jouw webpagina er nu zo uit:
+                            </p>
+                        </article>
+                    </>
+                </main>
+            </header>
+        </>
     );
 }
 
